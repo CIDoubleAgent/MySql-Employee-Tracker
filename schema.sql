a78b781db395eca5dd9e31abb94fa07e147c3077
Nick Zabkar <nzabkar51@gmail.com>
2021-09-24 20:15:30 -0400

    Added seeds.sql file

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE departments(
 );
 
 CREATE TABLE roles(
-    id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     department_id INT,
     salary INT(6),
@@ -16,10 +16,9 @@ CREATE TABLE roles(
 );
 
 CREATE TABLE employees(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
