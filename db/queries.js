08221e57207b981b744c603b80fcdb8c386a8deb
Nick Zabkar <nzabkar51@gmail.com>
2021-09-26 23:16:03 -0400

    Added salary field to getAllEmployees query

--- db/queries.js
+++ db/queries.js
@@ -39,8 +39,9 @@ async function getAllEmployees() {
     `SELECT employees.id, 
     employees.first_name, 
     employees.last_name, 
-    roles.title AS title, 
-    departments.name AS department FROM employees 
+    roles.title, 
+    departments.name AS department, 
+    roles.salary FROM employees 
     LEFT JOIN roles ON employees.role_id = roles.id 
     LEFT JOIN departments ON roles.department_id = departments.id`
   );
